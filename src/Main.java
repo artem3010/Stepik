6b7ed1e58cddb3f2ae2904d708d9a554823fa960
Oleg <Ile813@yandex.ru>
2022-05-29 22:27:26 +0500

    first_task_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Введите булевы значения: ");
+        Scanner in = new Scanner(System.in);
+        int year = Integer.parseInt(in.nextLine());
+        int res  = Main.leapYearCount(year);
+        System.out.println(res);
+    }
+    public static int leapYearCount(int year) {
+        return year/4 - year/100 + year/400;
     }
 }
