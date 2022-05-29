9776591e5d724ea95dcd31f71c20e4ab0e53e497
Oleg <Ile813@yandex.ru>
2022-05-29 22:36:29 +0500

    commit_task_7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Это второй модуль!");
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+        boolean res = Main.isPalindrome(text);
+        System.out.println(res);
+    }
+    public static boolean isPalindrome(String text) {
+        String newString = text.replaceAll("[^a-zA-Z0-9]","");
+        StringBuilder sb_newString = new StringBuilder(newString);
+        boolean fin = newString.equalsIgnoreCase(sb_newString.reverse().toString());
+        return fin;
     }
 }
