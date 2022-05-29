c76190e4f194eb6623a0606267675203ba5ee273
Oleg <Ile813@yandex.ru>
2022-05-29 22:25:18 +0500

    first_task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Введите булевы значения: ");
+        Scanner in = new Scanner(System.in);
+        boolean a = Boolean.parseBoolean(in.nextLine());
+        boolean b = Boolean.parseBoolean(in.nextLine());
+        boolean c = Boolean.parseBoolean(in.nextLine());
+        boolean d = Boolean.parseBoolean(in.nextLine());
+        boolean res = Main.booleanExpression(a, b, c, d);
+        System.out.println(res);
+    }
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        return (a ^ b)&(c ^  d) || (a ^  c) & (b ^  d);
     }
 }
