4b561013dc1f8c553e77a6f2274950bb46675efb
Oleg <Ile813@yandex.ru>
2022-05-29 22:35:06 +0500

    commit_task_6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Это второй модуль!");
+        Scanner in = new Scanner(System.in);
+        int value = Integer.parseInt(in.nextLine());
+        boolean res = Main.isPowerOfTwo(value);
+        System.out.println(res);
+    }
+    public static boolean isPowerOfTwo(int value) {
+        int a = Math.abs(value);
+        Integer b = Integer.bitCount(a);
+        return b == 1 ? true : false;
     }
 }
