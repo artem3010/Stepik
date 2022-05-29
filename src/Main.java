df555064bcd551868fa747fb13b0895e9d789aba
Oleg <Ile813@yandex.ru>
2022-05-29 22:32:57 +0500

    commit_task_3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Это второй модуль!");
+        Scanner in = new Scanner(System.in);
+        int value = Integer.parseInt(in.nextLine());
+        int bitIndex = Integer.parseInt(in.nextLine());
+        int res = Main.flipBit(value, bitIndex);
+        System.out.println(res);
+    }
+    public static int flipBit(int value, int bitIndex) {
+        return value ^ (1 << bitIndex-1);
     }
 }
