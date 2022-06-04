a97d2d40a8cde0316cc9361da402dffa1fb028fa
Oleg <Ile813@yandex.ru>
2022-06-04 22:29:26 +0500

    commit_module_3_task_3.4(1)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-import java.util.Scanner;
-import java.util.function.DoubleUnaryOperator;
-public class Main {
-    public static void main(String[] args) {
-        double a = new Scanner(System.in).nextDouble();
-        double b = new Scanner(System.in).nextDouble();
-        System.out.println();
+public class AsciiCharSequence implements CharSequence {
+
+    private byte[] bytes;
+
+    AsciiCharSequence(byte[] bytes) {
+        this.bytes = bytes;
     }
-    public static double integrate(DoubleUnaryOperator f, double a, double b) {
-        double I = 0;
-        while (a < b) {
-            I += f.applyAsDouble(a += 1e-6);
-        }
-        return I*1e-6;
+
+    @Override
+    public int length() {
+        return bytes.length;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return (char) bytes[index];
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return new AsciiCharSequence(Arrays.copyOfRange(bytes, start, end));
+    }
+
+    @Override
+    public String toString() {
+        return new String(bytes);
     }
 }
-
