97156bf6fed72bc840fb6e22e804872ca3150897
Oleg <Ile813@yandex.ru>
2022-06-04 22:52:19 +0500

    commit_module_3_task_3.5(1)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 import java.util.Scanner;
 import java.util.function.DoubleUnaryOperator;
 public class Main {
-    public static void main(String[] args) {
-        double a = new Scanner(System.in).nextDouble();
-        double b = new Scanner(System.in).nextDouble();
-        System.out.println();
-    }
-    public static double integrate(DoubleUnaryOperator f, double a, double b) {
-        double I = 0;
-        while (a < b) {
-            I += f.applyAsDouble(a += 1e-6);
-        }
-        return I*1e-6;
-    }
+
 }
 
