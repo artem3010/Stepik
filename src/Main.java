bea2a4ee987a43953b00c8b272da7a3d14c3b72e
Oleg <Ile813@yandex.ru>
2022-06-04 20:51:42 +0500

    commit_module_3_task_3.2(3)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Objects;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Это модуль 3");
+    public final class ComplexNumber {
+        private final double re;
+        private final double im;
+
+        public ComplexNumber(double re, double im) {
+            this.re = re;
+            this.im = im;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ComplexNumber that = (ComplexNumber) o;
+            return Double.compare(that.getRe(), getRe()) == 0 && Double.compare(that.getIm(), getIm()) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getRe(), getIm());
+        }
+
+        public double getRe() {
+            return re;
+        }
+
+        public double getIm() {
+            return im;
+        }
     }
 }
