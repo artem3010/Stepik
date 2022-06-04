5a36e846142a3d6799dec631ddfe123fbe5196f5
Oleg <Ile813@yandex.ru>
2022-06-04 17:19:36 +0500

    commit_module_3_task_3.2(2)

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,5 @@
-import java.util.Scanner;
 public class Main {
-    public static void main(String[] args){
-        System.out.print("Введите конечные координат робота: \nX = ");
-        int toX = new Scanner(System.in).nextInt();
-        System.out.print("Y = ");
-        int toY = new Scanner(System.in).nextInt();
-        Robot Tom = new Robot();
-        Main.moveRobot(Tom, toX, toY);
-    }
-    public static void moveRobot(Robot robot, int toX, int toY) {
-
-        if (robot.getY() < toY) {
-            while (robot.getDirection() != Direction.UP) robot.turnRight();
-            while (robot.getY() < toY) robot.stepForward();
-            System.out.println("Yкон = " + robot.getY());
-        } else if (robot.getY() > toY) {
-            while (robot.getDirection() != Direction.DOWN) robot.turnRight();
-            while (robot.getY() > toY) robot.stepForward();
-            System.out.println("Yкон = " + robot.getY());
-        }
-        if (robot.getX() < toX) {
-            while (robot.getDirection() != Direction.RIGHT) robot.turnRight();
-            while (robot.getX() < toX) robot.stepForward();
-            System.out.print("Xкон = " + robot.getX());
-        } else if (robot.getX() > toX) {
-            while (robot.getDirection() != Direction.LEFT) robot.turnRight();
-            while (robot.getX() > toX) robot.stepForward();
-            System.out.print("Xкон = " + robot.getX());
-        }
+    public static void main(String[] args) {
+        System.out.println("Это модуль 3");
     }
 }
-
