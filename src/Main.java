88f6699f531d9c273da527041299d2481dbcda1c
Oleg <Ile813@yandex.ru>
2022-06-04 16:27:19 +0500

    commit_module_3_task_3.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Это модуль 3");
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        Scanner in = new Scanner(System.in);
+        toX = new Scanner(System.in).nextInt();
+        toY = new Scanner(System.in).nextInt();
+        if (robot.getY() < toY) {
+            while (robot.getDirection() != Direction.UP) robot.turnRight();
+            while (robot.getY() < toY) robot.stepForward();
+        } else if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) robot.turnRight();
+            while (robot.getY() > toY) robot.stepForward();
+        }
+        if (robot.getX() < toX) {
+            while (robot.getDirection() != Direction.RIGHT) robot.turnRight();
+            while (robot.getX() < toX) robot.stepForward();
+        } else if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) robot.turnRight();
+            while (robot.getX() > toX) robot.stepForward();
+        }
     }
 }
+
