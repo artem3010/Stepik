c600efaad13443319026665d20e31c7995d0242f
Oleg <Ile813@yandex.ru>
2022-06-18 20:16:14 +0500

    commit_module_4_task_4.3(first_commit)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        boolean success = false;
+        for (int i = 0; !success && (i < 3); ++i) {
+            try (RobotConnection connection = robotConnectionManager.getConnection()) {
+                connection.moveRobotTo(toX, toY);
+                success = true;
+            } catch (RobotConnectionException e) {
+            }
+        }
+        if (!success) {
+            throw new RobotConnectionException("The END. No more attempts.");
+        }
     }
 }
