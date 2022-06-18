333607492beb5bfc22a95b9bfd9a225228def62f
Oleg <Ile813@yandex.ru>
2022-06-18 19:39:35 +0500

    commit_module_4_task_4.2(first_commit)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getCallerClassAndMethodName.getCallerClassAndMethodName());
+        anotherMethod();
+        otherMethod();
+
     }
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName.getCallerClassAndMethodName());
+
+    }
+    private static void otherMethod() {
+        System.out.println(getCallerClassAndMethodName.getCallerClassAndMethodName());
+    }
+
 }
