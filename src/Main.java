d2539c2b03e139411d124aecceb900d37de493c9
Oleg <Ile813@yandex.ru>
2022-06-18 19:15:50 +0500

    commit_module_4_task_4.1(first)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean isExit = false;
+        while (!isExit) {
+            System.out.println(sqrt(new Scanner(System.in).nextDouble()));
+            isExit = new Scanner(System.in).toString().equals("close");
+        }
+
+
+
+    }
+    public static double sqrt(double x) {
+        if (x >= 0){
+            return Math.sqrt(x);
+        } throw new java.lang.IllegalArgumentException ("Expected non-negative number, got "+ x);
     }
 }
